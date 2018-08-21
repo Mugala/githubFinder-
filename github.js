91a1d91298987e30f21b3428a59194ffb92d4251
mugala <mugalalevi21@gmail.com>
2018-08-21 22:21:29 +0300

    Get User Method This commit will create an async function for getting the user profile

--- github.js
+++ github.js
@@ -3,4 +3,8 @@ class GitHub {
     this.client_id = '865f88da5f9ccdcb077a';
     this.client_secret = '36bb92a36bc9c90b76cbd872bdb30394e7f4f4e6';
   }
+
+  async getUser(user){
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  }
 }
