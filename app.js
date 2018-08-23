955d402f261240a6d24c6d3162ae0802235907c6
mugala <mugalalevi21@gmail.com>
2018-08-23 14:41:58 +0300

    Initialize the github class This commit will init the gihub class in app.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-// search input
+//Init Github
+
+const github = new GitHub
+
+// Search input
+
 const searchUser = document.getElementById('searchUser');
 
-//Search Inputs event Listener
+//Search inputs event listener
 
 searchUser.addEventListener('keyup', (e) => {
   //Get input text
   const userText = e.target.value;
 
-  if(userText !==''){
-    console.log(userText);
+  if(userText !== ''){
+    // Make http call
+    github.getUser(userText)
+    .then(data => {
+      console.log(data);
+    })
   }
+
 });
